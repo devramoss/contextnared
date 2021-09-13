fa36e61a37ded85063d1317548f8853611bea6f9
Emilly Ramos <devramoss2003@gmail.com>
2021-09-13 18:30:09 -0300

    Separating responsibilities

--- src/components/ContextBox/index.js
+++ src/components/ContextBox/index.js
@@ -1,29 +1,10 @@
 import React,{useState, useEffect} from 'react';
-
+import getAllContexts from '../../services/dato-cms';
 function ContextBox(){
-    const API_URL = 'https://graphql.datocms.com/';
-    const API_TOKEN = '610cc2ac61382dd6da80be37f8257a';
     const [context, setContext] = useState([]);
     useEffect(()=>{
-        fetch(API_URL, {
-                method: 'POST', 
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Authorization': `Bearer ${API_TOKEN}`,
-                },
-                body: JSON.stringify({
-                    query : `{
-                        allContextos {
-                        id
-                        autor
-                        texto
-                        }
-                    
-                    }`
-                }),
-            })
-            .then((response) => {return response.json()})
+        
+            getAllContexts()
             .then((completeResponse)=>{
                 const posts = completeResponse.data.allContextos
                 setContext(posts);
