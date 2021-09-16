b29bffdac15ba73c8570219f9aaf80668fa983de
Emilly Ramos <devramoss2003@gmail.com>
2021-09-15 21:28:35 -0300

    Styling the about page

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import font from '../../services/fonts.css';
-import Form from '../../components/Form';
+import Form from './Component/Form';
 import Header from '../../components/Header';
 import '../Sobre/style.css';
 
@@ -8,11 +8,15 @@ function SobrePage(){
     return(
         <div className = "boxSobre">
             <Header/>
-            <h1 className = "title">Sobre</h1>
-            <p>O ContextNaRed nasceu com o intuito de ajudar outros estudantes</p>
-            <p>Deseja bater um papo com a gente ? Entre em contato</p>
-            <h2 className = "title">Contato</h2>
-            <Form/>
+            <main class = "about__us">
+                <h1 className = "title">Sobre</h1>
+                <p>
+                    O ContextNaRed nasceu com o intuito de ajudar outros estudantes.
+                    Deseja bater um papo ? Entre em contato conosco pelo formulário pelo formulário abaixo.
+                </p>
+                <h2 className = "title">Contato</h2>
+                <Form/>
+            </main>
       </div>
       
     )
