9aefacdc9ca573847b31bac96d6e3301e01ff89b
Emilly Ramos <devramoss2003@gmail.com>
2021-09-16 19:51:54 -0300

    Creating the page not found (404)

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch} from 'react-router-dom';
 import HomePage from './pages/Home';
 import SobrePage from './pages/Sobre';
+import Page_404 from './pages/Page__404';
 
 function MainRoutes(){
     return(
         <Switch>
             <Route exact path = "/" component = {HomePage}/>
-            <Route path = "/sobre" component = {SobrePage}/>
+            <Route exact path = "/sobre" component = {SobrePage}/>
+            <Route component = {Page_404}/>
         </Switch>
     )
 }
