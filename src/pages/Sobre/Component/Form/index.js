84f073c94ec00f816e32e249b29aad1ee1c4228f
Emilly Ramos <devramoss2003@gmail.com>
2021-10-04 14:28:04 -0300

    Setting up form submission

--- src/pages/Sobre/Component/Form/index.js
+++ src/pages/Sobre/Component/Form/index.js
@@ -43,7 +43,7 @@ function Form(){
         },
         validate,
         onSubmit: (values) => {
-            api.post("enviarformulario", {
+            api.post('/enviarformulario', {
                 nome: values.name,
                 email: values.email,
                 assunto: values.subject
